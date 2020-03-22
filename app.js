7fe24de69299151636ccc3e2043a93ad56264043
Frank Flores <frankthetank67@gmail.com>
2020-03-21 20:33:54 -0700

    added a rating label to each image. added .clear method to clear the contents of results div everytime a new animal button is clicked.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // search bar or input for user to enter a keyterm
 // preset list of terms for user to click on
-var searchTerms = ["cat", "dog", "penguin", "mouse", "monkey"];
+var topics = ["cat", "dog", "penguin", "mouse", "monkey"];
 
-for (var i=0; i < searchTerms.length; i++) {
-    $("#btn-div").append(`<button class="btn btn-secondary">${searchTerms[i]}</button>`)
+for (var i=0; i < topics.length; i++) {
+    $("#btn-div").append(`<button class="btn btn-secondary">${topics[i]}</button>`)
 }
 // when button is clicked take the value of button and send to search for that term
-$(document).on("click", '.btn', function() {
+$(document).on("click", '.btn-secondary', function() {
+    $("#results").empty();
     var text = $(this).text();
     console.log($(this).text());
     getGiphy(text);
 })
+
+
 
 function getGiphy(term) {
     var endPoint = 'https://api.giphy.com/v1/gifs/search?';
@@ -49,12 +52,18 @@ console.log($(this).attr("data-gif"));
 function appendGiphy(data) {
 
     for (var i=0; i < data.length; i++) {
-        $("#results").append(`<img style='width:20%' 
+        $("#results").prepend(`<label>Rating: ${data[i].rating}</label> <img style='width:20%' 
         data-status='still'
         data-still='${data[i].images['fixed_height_small_still'].url}'
         data-gif='${data[i].images['fixed_width'].url}'
         src='${data[i].images['fixed_height_small_still'].url}'>`)
+        
+        console.log(data[i].rating);
     }
 
 }
+
+$("#btn-submit").on("click", function() {
+    $("")
+})
 // 
